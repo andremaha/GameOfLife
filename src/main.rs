862fa7b08cca1448950c0ed63f2e68a75b7446b2
Andrey Esaulov <esaulov.andrey@gmail.com>
2021-07-23 11:29:53 +0200

    finished the iteration containing cli arguments, reading and writing to files

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,116 @@
 extern crate rand;
-use std::{thread, time};
+extern crate termion;
+use std::{thread, time, env};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use termion::clear;
+use std::io::prelude::*;
 
 fn main() {
     let mut world = [[0u8; 75]; 75];
     let mut generations = 0;
 
+    let args: Vec<String> = env::args().collect();
     
+    // No file provided to populate the world - use random population
+    if args.len() < 2 {
+        for i in 0..74 {
+            for j in 0..74 {
+                if rand::random() {
+                    world[i][j] = 1;
+                } else {
+                    world[i][j] = 0;
+                }
+            }
+        }
+    } else {
+        let filename = env::args().nth(1).unwrap();
+        world = populate_from_file(filename);
+    }
+
+    for _gens in 0..10 {
+        let temp = generation(world);
+        world = temp;
+        generations += 1;
+        println!("{}", clear::All);
+        displayworld(world);
+        println!("Population at generation {g} is {c}", g = generations, c = census(world));
+        thread::sleep(time::Duration::from_secs(2));
+    }
+
+    // Save the last generation to file as field
+    write_world_to_file(world, String::from("last_field.txt"));
+    // Save the last generatio to file as coorinates
+    write_world_coordinates_to_file(world, String::from("world.txt"));
+
+}
+
+
+/// # Writes the current world as the filed to a file
+fn write_world_to_file(world: [[u8; 75]; 75], filename: String) {
+    let mut file = File::create(filename).unwrap();
     for i in 0..74 {
         for j in 0..74 {
-            if rand::random() {
-                world[i][j] = 1;
+            if world[i][j] == 1 {
+                write!(file, "{}", "*").unwrap();
             } else {
-                world[i][j] = 0;
+                write!(file, "{}", " ").unwrap();
+            }
+            
+        }
+        write!(file, "{}", "\n").unwrap();
+    }
+
+}
+
+/// # Writes the current world into the coordinates file
+/// 
+/// The file can be used to regenerate the field 
+/// By passing filename as an argument when running the program
+fn write_world_coordinates_to_file(world: [[u8; 75]; 75], filename: String) {
+    let mut file = File::create(filename).unwrap();
+    for i in 0..74 {
+        for j in 0..74 {
+            if world[i][j] == 1 {
+                write!(file, "{} {}\n", i, j).unwrap();
             }
         }
     }
 
-    //println!("{:?}", world);
-    println!("Living cells in the OLD world: {:?}", census(world));
+}
 
-    let newworld = generation(world);
-    //println!("{:?}", newworld);
-    println!("Living cells in the NEW world: {:?}", census(newworld));
+/// # Creates a world from coordinates in the file
+/// 
+/// File format with coordinates of the cells in the field that have life in them
+/// 0 5
+/// 0 7
+/// 0 18
+fn populate_from_file(filename: String) -> [[u8; 75]; 75] {
+    let mut newworld = [[0u8; 75]; 75];
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    let mut pairs: Vec<(usize, usize)> = Vec::new();
+    for (_index, line) in reader.lines().enumerate() {
+        let l = line.unwrap();
+        let mut words = l.split_whitespace();
+        let left = words.next().unwrap();
+        let right = words.next().unwrap();
+        pairs.push((left.parse::<usize>().unwrap(), right.parse::<usize>().unwrap()));
+    }
+
+    for i in 0..74 {
+        for j in 0..74 {
+            newworld[i][j] = 0;
+        }
+    }
+
+    println!("{:?}", pairs);
+
+    for (x, y) in pairs {
+        newworld[x][y] = 1;
+    }
+
+    newworld
 }
 
 /// # Counts all living cells 
@@ -135,4 +224,19 @@ fn generation(world: [[u8; 75]; 75]) -> [[u8; 75]; 75] {
     }
 
     newworld
+}
+
+/// # Display the current evolution of the world
+/// 
+fn displayworld(world: [[u8; 75]; 75]) {
+    for i in 0..74 {
+        for j in 0..74 {
+            if world[i][j] == 1 {
+                print!("*");
+            } else {
+                print!(" ");
+            }
+        }
+        println!("");
+    }
 }
